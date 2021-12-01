40ba8b78643e29847dafaf300fbd443be3d7f383
MuffinPlease <benjamintec3455@gmail.com>
2021-12-01 11:31:37 -0600

    GeoF Constants to Functions

--- figures.js
+++ figures.js
@@ -1,42 +1,25 @@
-//Square code
+//Square code----------------------------
 console.group("Square");
-const sideSquare = 5;
-console.log(`The sides of the square measure: ${sideSquare} cm`);
 
-const perimeterSquare = sideSquare*4;
-console.log(`The perimeter of the square is: ${perimeterSquare} cm`);
+const perimeterSquare = x =>(x*4);
 
-const areaSquare = sideSquare*sideSquare;
-console.log(`The area of the square is: ${areaSquare} cm^2`);
+const areaSquare = x => (x*x);
 console.groupEnd();
 
-//Triangle code
+//Triangle code----------------------------
 console.group("Triangle");
-const sideTriangleA = 6;
-const sideTriangleBase = 4;
-const sideTriangleC = 6;
 
-console.log(`The sides of the Triangle measure: ${sideTriangleA} cm, ${sideTriangleBase} cm, ${sideTriangleC} cm`);
+const perimeterTriangle = (a,b,c) => (a+b+c);
 
-const heightTriangle = 5.5;
-console.log(`The height of the Triangle measure: ${heightTriangle} cm`);
+const areaTriangle = (b,h) => (b*h/2);
 
-
-const perimeterTriangle = sideTriangleA+sideTriangleBase+sideTriangleC;
-console.log(`The perimeter of the Triangle is: ${perimeterTriangle} cm`);
-
-const areaTriangle = (sideTriangleBase*heightTriangle)/2;
-console.log(`The area of the Triangle is: ${areaTriangle} cm^2`);
 console.groupEnd();
 
-//Circle code
+//Circle code----------------------------
 console.group("Circle");
-const radiusCircle = 4;
-console.log(`The radius of the Circle measure: ${radiusCircle} cm`);
 
-const circumferenceCircle = 2*radiusCircle*Math.PI;
-console.log(`The circumference of the Circle is: ${circumferenceCircle} cm`);
+const circumferenceCircle = (r) => (2*r*Math.PI);
 
-const areaCircle = (radiusCircle*radiusCircle)*Math.PI;
-console.log(`The area of the Circle is: ${areaCircle} cm^2`);
+const areaCircle = (r) => (r*r)*Math.PI;
+
 console.groupEnd();
