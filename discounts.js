24c403ca7a2d91a751516084a2b94d32a8488be0
MuffinPlease <benjamintec3455@gmail.com>
2021-12-02 10:19:45 -0600

    Ds Function to discounting

--- discounts.js
+++ discounts.js
@@ -1,1 +1,18 @@
-console.log("Hello word");
+const originalPrice = 120;
+const discount = 18;
+
+const porcentageWithDiscount = 100 - discount;
+const priceWithDiscount = (originalPrice * porcentageWithDiscount)/100;
+
+// console.log({
+//     originalPrice,
+//     discount,
+//     porcentageWithDiscount,
+//     priceWithDiscount
+// });
+
+const calculatePriceWithDiscount = (p,d) =>(
+    //p -> Price
+    //d -> Discount
+    (p * (100-d) / 100)
+);
